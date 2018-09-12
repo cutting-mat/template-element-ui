bf4f89c173020337adaef5f42f2a5fce543d9e7e
tower1229 <tower1229@foxmail.com>
2018-09-12 17:23:29 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -45,12 +45,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url(assets/common.css);
 </style>
