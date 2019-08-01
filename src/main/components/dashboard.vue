a9e2e5ee90c543c3bdfe205e29c526636d78d77d
tower1229 <tower1229@foxmail.com>
2019-08-01 18:14:23 +0800

    update

--- src/main/components/dashboard.vue
+++ src/main/components/dashboard.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="wrap">
-    dashboard
+  <div class="scrollbar">
+    <div class="flex-row" style="margin-top: 20px;">
+      <div class="flex-1 box blockLayout"></div>
+      <div class="flex-1 box blockLayout"></div>
+      <div class="flex-1 box blockLayout"></div>
+    </div>
+    <div class="flex-row">
+      <div class="flex-1 box blockLayout"></div>
+      <div class="flex-1 box blockLayout"></div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +19,7 @@ export default {
 </script>
 
 <style scoped>
-
+.box{
+  height: 250px;
+}
 </style>
