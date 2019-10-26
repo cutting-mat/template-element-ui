b857985d0bbe13aa9ff1a63a48b50bbf8c63b99e
tower1229 <tower1229@foxmail.com>
2019-10-26 10:22:18 +0800

    update

--- src/store.js
+++ src/store.js
@@ -1,20 +1,23 @@
 export const store = {
     debug: true,
     state: {
-        tokenInfo: {},          // token携带信息
-        menu: [],               // 导航菜单
-        user: {},               // 用户信息
-        isCollapse: false,      // 菜单收起状态 
-        permission: []          // 用户权限
+        tokenInfo: {}, // token携带信息
+        menu: [], // 导航菜单
+        user: {}, // 用户信息
+        isCollapse: false, // 菜单收起状态 
+        permission: [] // 用户权限
     },
     set(key, newValue) {
-        if(this.state[key]!==void(0)){
-            this.state[key] = newValue;
+        return new Promise((resolve, reject) => {
+            if (this.state[key] !== void(0)) {
+                this.state[key] = newValue;
 
-            this.debug && console.log('store update:', key, '=>',  this.state[key])
-        }else{
-            console.warn(`this key [${key}] is not register in store!`)
-        }
+                this.debug && console.log('store update:', key, '=>', this.state[key])
+                resolve(true)
+            } else {
+                reject(`this key [${key}] is not register in store!`)
+            }
+        })
     },
     get(key) {
         return new Promise((resolve, reject) => {
@@ -23,7 +26,7 @@ export const store = {
                     if (this.state[key].length) {
                         resolve(this.state[key])
                     } else {
-                        
+
                     }
                     break;
                 default:
