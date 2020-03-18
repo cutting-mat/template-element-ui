a82e8fade1834c7cb8fba6303375c9645eca1a01
tower1229 <tower1229@foxmail.com>
2020-03-18 11:18:17 +0800

    store添加action方法，引入ajax并发队列机制

--- src/store.js
+++ src/store.js
@@ -1,3 +1,10 @@
+import Vue from 'vue'
+const bus = new Vue();
+
+// 队列容器
+let promiseQueue = {};
+
+
 export const store = {
     debug: true,
     state: {
@@ -21,23 +28,71 @@ export const store = {
     },
     get(key) {
         return new Promise((resolve, reject) => {
-            switch (key) {
-                case "someKey":
-                    if (this.state[key].length) {
-                        resolve(this.state[key])
-                    } else {
-                        // ajax or do something
-                    }
-                    break;
-                default:
-                    if (this.state[key] !== void(0)) {
-                        resolve(this.state[key])
-                    } else {
-                        reject(`this key [${key}] is not register in store!`)
-                    }
+            if (this.state[key] !== void(0)) {
+                resolve(this.state[key])
+            } else {
+                reject(`key [${key}] is not register in store!`)
+            }
 
+        })
+    },
+    checkStore(key, type) {
+        let result;
+        type = type || 'array'; // array,object,number,string,boolean
+        switch (type) {
+            case 'array':
+                result = !!this.state[key].length
+                break;
+            case 'object':
+                result = !!Object.keys(this.state[key]).length
+                break;
+            default:
+                result = !!this.state[key]
+        }
+        return result
+    },
+    action(key, type) {
+        return new Promise((resolve, reject) => {
+            // action数据集中处理
+            const catchActionData = (data) => {
+                this.set(key, data)
+                resolve(data)
+                delete promiseQueue[key];
+            }
+            if (this.checkStore(key, type)) {
+                resolve(this.state[key])
+            } else {
+                // 检测并加入队列
+                if(promiseQueue[key]){
+                    bus.$once(promiseQueue[key], (msg) => {
+                        catchActionData(msg)
+                    })
+                }else{
+                    // 创建队列
+                    promiseQueue[key] = 'action_' + parseInt(Math.random() * 1e8);
+
+                    switch (key) {
+                        case "someKey":
+                            // send ajax and use `catchActionData()` to catch data!
+                            // fetchSomeKey().then(res => {
+                            //     catchActionData(res.data)
+                            // })
+                            break;
+                        
+                        default:
+                            reject(`key [${key}] has not define an action!`)
+                    }
+                }
             }
         })
+    }
+}
 
+// 控制台查看工具
+if(store.debug){
+    window.store = {
+        list() {
+            return JSON.parse(JSON.stringify(store.state))
+        }
     }
 }
