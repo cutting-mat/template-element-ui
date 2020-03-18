219a94c1110e99b7662227d2389441dedb15c944
tower1229 <tower1229@foxmail.com>
2020-03-18 16:42:34 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,14 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     'eslint:recommended'
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off'
   }
 }
