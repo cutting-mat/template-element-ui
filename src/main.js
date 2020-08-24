7329afbba6156b73bd59522398919a2598b5d923
tower1229 <tower1229@foxmail.com>
2020-08-24 13:33:04 +0800

    fix: remove core-js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 // 根据package.json [browserslist]配置加载polyfill，需同时替换babel.config.js中注释的presets配置
-import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import Vue from 'vue'
@@ -15,25 +14,25 @@ Vue.use(register);
 import router from './router'
 
 /*
-* 注册 v-has 指令（权限控制）
-*/
+ * 注册 v-has 指令（权限控制）
+ */
 
 Vue.directive('has', {
-  inserted: function(el, binding) {
-    if (Vue.prototype.$_has && !Vue.prototype.$_has(binding.value)) {
-      el.parentNode.removeChild(el);
+    inserted: function(el, binding) {
+        if (Vue.prototype.$_has && !Vue.prototype.$_has(binding.value)) {
+            el.parentNode.removeChild(el);
+        }
     }
-  }
 });
 
 import App from './App.vue'
 
 new Vue({
-  data(){
-    return {
-      AccessControl: true
-    }
-  },
-  router,
-  render: h => h(App)
+    data() {
+        return {
+            AccessControl: true
+        }
+    },
+    router,
+    render: h => h(App)
 }).$mount('#app')
