c216d2a935afe8b89897d76111a435320a0491ca
tower1229 <tower1229@foxmail.com>
2020-08-25 09:03:18 +0800

    style: 格式化

--- src/api.js
+++ src/api.js
@@ -2,49 +2,49 @@ import axios from 'axios';
 import * as util from '@/common/assets/util';
 
 const HOST = {
-  mock: 'http://rap2api.taobao.org/app/mock/223572',
-  dev: 'http://172.17.2.1:8187',
-  test: '//test.com/api',
-  master: '//master.com/api'
+    mock: 'http://rap2api.taobao.org/app/mock/8',
+    dev: 'http://172.17.2.1:8187',
+    test: '//test.com/api',
+    master: '//master.com/api'
 };
 
 const HASH = {
-  "test.com": HOST.test,
-  "master.com" : HOST.master
+    "test.com": HOST.test,
+    "master.com": HOST.master
 }
 
 export const baseURL = HASH[window.location.host] || HOST.mock;
 
 export const instance = axios.create({
-  baseURL,
-  timeout: 10000,
-  headers: {
-    'Content-Type':'application/json'
-  }
+    baseURL,
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json'
+    }
 });
 
 // 请求处理
-instance.interceptors.request.use(function (config) {
-  return config;
-}, function (error) {
-  return Promise.reject(error);
+instance.interceptors.request.use(function(config) {
+    return config;
+}, function(error) {
+    return Promise.reject(error);
 });
 
 //错误处理
 instance.interceptors.response.use(function(response) {
-  if(response.status!=200){
-    return util.catchError(response);
-  }
-  // token续期
-  if(response.headers['jwt-update-token']){
-    util.emit('login', {
-      silent: true,
-      data: {
-        accessToken: response.headers['jwt-update-token']
-      }
-    })
-  }
-  return response;
-}, function (error) {
-  return util.catchError(error);
+    if (response.status != 200) {
+        return util.catchError(response);
+    }
+    // token续期
+    if (response.headers['jwt-update-token']) {
+        util.emit('login', {
+            silent: true,
+            data: {
+                accessToken: response.headers['jwt-update-token']
+            }
+        })
+    }
+    return response;
+}, function(error) {
+    return util.catchError(error);
 });
