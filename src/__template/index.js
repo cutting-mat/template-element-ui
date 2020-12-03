712d19d4ed5f5870e285c9fdf3e022259be37596
tower1229 <tower1229@foxmail.com>
2020-12-03 17:58:39 +0800

    style:
    新增演示路由示例

--- src/__template/index.js
+++ src/__template/index.js
@@ -2,9 +2,16 @@ export default [{
     path: '/template',
     name: '空白模块',
     component: (resolve) => require(['./views/Index.vue'], resolve),
+    redirect: '/template/list',
     children: [{
         path: 'list',
         name: '列表',
-        component: (resolve) => require(['./views/List.vue'], resolve)
+        component: (resolve) => require(['./views/List.vue'], resolve),
+        meta: {
+            icon: '',                   // 字体图标
+            title: '',                  // 展示名
+            hide: true,                 // 在导航中隐藏
+            belong: 'ROUTE NAME'        // 导航当前状态归属
+        }
     }]
 }]
