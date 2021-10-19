15351a7fc03554b1cdf04a07fee1aebfd3f242a8
tower1229 <tower1229@foxmail.com>
2021-10-19 11:16:29 +0800

    更新组件库依赖

--- src/core/widget-support/report.js
+++ src/core/widget-support/report.js
@@ -1,17 +1,40 @@
-let widgetAmount = {}
+let widStore = {};
 
-export const report = function(packageInfo) {
-    if (packageInfo) {
-        const widgetKey = `${packageInfo.name}__${packageInfo.version}`;
-        let routeAmount = widgetAmount[widgetKey] || [];
-        routeAmount.push(this.$route.fullPath)
-        widgetAmount[widgetKey] = [...new Set(routeAmount)];
-        // 上报
-        // {widgetKey: [path, ...], ...}
-        console.warn('组件上报', {
-            appId: 1,
-            timestamp: new Date().getTime(),
-            amount: widgetAmount
-        })
+export const report = {
+    widgetKey: null,
+    pageHref: null,
+    pageInstanceId: null,
+    send: function (packageInfo) {
+        if (packageInfo) {
+            this.init(packageInfo);
+
+            let widgetAmount = {
+                appId: 1,
+                timestamp: new Date().getTime(),
+                widgetKey: this.widgetKey,
+                pageHref: this.pageHref,
+                pageInstanceId: this.pageInstanceId
+            }
+
+            // 上报
+            console.warn('组件上报', widgetAmount)
+        }
+    },
+    init: function (packageInfo) {
+        if (packageInfo) {
+            this.widgetKey = `${packageInfo.name}__${packageInfo.version}`;
+            this.pageHref = window.location.href;
+            if (!widStore[this.pageHref]) {
+                widStore[this.pageHref] = {}
+            }
+            if (!widStore[this.pageHref][this.widgetKey]) {
+                widStore[this.pageHref][this.widgetKey] = 1;
+            }else {
+                widStore[this.pageHref][this.widgetKey]++;
+            }
+            this.pageInstanceId = widStore[this.pageHref][this.widgetKey];
+            return this.pageInstanceId;
+        }
+
     }
 }
