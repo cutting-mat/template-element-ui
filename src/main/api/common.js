fd68d620536595fedfb475583817bb99389d033b
tower1229 <tower1229@foxmail.com>
2022-01-26 09:40:55 +0800

    更新公共api

--- src/main/api/common.js
+++ src/main/api/common.js
@@ -1,14 +1,21 @@
 import { instance } from '@/core';
 
-
-//上传图片base64
+// 上传图片base64
 export const uploadImg = params => {
     return instance.post(`/file/upload/base64`, params)
 }
 
-//文件上传地址
+// 上传文件
 export const upload = params => {
     return instance.post(`/file/upload`, params, {
         headers: { "Content-Type": "multipart/form-data" }
     })
+}
+// 上传富文本
+export const saveText = params => {
+    return instance.post(`/oss/${params.id || 'text'}`, params)
+}
+// 获取富文本详情
+export const getText = params => {
+    return instance.get(`/oss/${params}`)
 }
