9777d8579fe1afc2dd9b68e8c2756cd141a009d8
tower1229 <tower1229@foxmail.com>
2022-02-10 14:44:43 +0800

    bug fix

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,9 @@ export default {
     this.$AccountAuth({
       loginCallback: () => {
         // 初始化用户信息
-        this.$store.action("user", true)
+        this.$store.action("user", {
+          cache: false
+        })
       },
     });
   },
