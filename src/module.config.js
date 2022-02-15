f4dee25899b561e423b9fe70648277c47329a473
tower1229 <tower1229@foxmail.com>
2022-02-15 10:53:25 +0800

    优化模块配置文件

--- src/module.config.js
+++ src/module.config.js
@@ -1,13 +1,16 @@
 /**
  * 模块配置文件
  * 必须输出 mainModule[Router Array] 和 subModules[Router Array]
- * 主模块的首页('/')及所有子模块路由受`account-auth`和`access-control`模块控制
- * 主模块的其余页面(如'/public')为公开页面，不受任何访问限制
+ * 主模块的首页('/')为主模板，所有子模块路由挂载为主页子路由，受`account-auth`和`access-control`模块控制
+ * 主模块的其余页面(如'/public')，不受主模板('/')限制，且不受任何访问限制
  * */ 
 
 // 主模块
-export { default as mainModule } from '@/main/index'
+import main from '@/main/index'
 
+export const mainModule = [
+    ...main,
+] 
 // 子模块
 import system from '@/system'
 import user from '@/user'
