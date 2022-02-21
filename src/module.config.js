dd7862edaa6e20574abb1aa776cb147cc9a82cae
tower1229 <tower1229@foxmail.com>
2022-02-21 09:33:05 +0800

    更新 模块配置文件注释

--- src/module.config.js
+++ src/module.config.js
@@ -1,8 +1,8 @@
 /**
  * 模块配置文件
- * 必须输出 mainModule[Router Array] 和 subModules[Router Array]
- * 主模块的首页('/')为主模板，所有子模块路由挂载为主页子路由，受`account-auth`和`access-control`模块控制
- * 主模块的其余页面(如'/public')，不受主模板('/')限制，且不受任何访问限制
+ * 约定1： 必须输出 mainModule[Router Array] 和 subModules[Router Array]
+ * 约定2： mainModule 的第一个对象是首页('/')路由，所有子模块将注册为首页的子路由
+ * 约定3： 首页及其子路由受`account-auth`和`access-control`模块控制，mainModule 的其余路由(如'/login')不受访问限制
  * */ 
 
 // 主模块
@@ -11,6 +11,7 @@ import main from '@/main/index'
 export const mainModule = [
     ...main,
 ] 
+
 // 子模块
 import system from '@/system'
 import user from '@/user'
