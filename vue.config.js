fff2e2a008cfb0b8447af095b61792a9992dbdcd
tower1229 <tower1229@foxmail.com>
2022-03-31 05:56:24 +0800

    修改注释

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,19 @@
  * vue-cli配置
  * */
 module.exports = {
-    transpileDependencies: ['@cutting-mat/axios', '@cutting-mat/uploader', 'tiny-emitter'],    // 需要babel编译的依赖包名
+    transpileDependencies: [                                    // 需要babel编译的依赖包名
+        '@cutting-mat/axios',
+        '@cutting-mat/uploader',
+        'tiny-emitter'
+    ],
     css: {
         sourceMap: true                                         // 开启css map, 方便调试
     },
     productionSourceMap: false,                                 // 生产环境关闭map
-    integrity: process.env.NODE_ENV === 'production',           // 子资源完整性校验（SRI）
+    integrity: process.env.NODE_ENV === 'production',           // 生产环境开启子资源完整性校验（SRI）
     outputDir: 'dist',                                          // 构建目录, 默认'dist'
-    publicPath: process.env.NODE_ENV === 'production'
-        //? '/template-element-ui'                                                   // 生产环境构建路径, 默认'/'
+    publicPath: process.env.NODE_ENV === 'production'           // 生产/开发环境构建路径, 默认'/'
+        //? '/template-element-ui'                                                   
         ? '/'
         : '/',
     chainWebpack(config) {
@@ -35,8 +39,8 @@ module.exports = {
                                     priority: 10,
                                     chunks: 'initial' // only package third parties that are initially dependent
                                 },
-                                elementUI: {
-                                    name: 'chunk-elementUI', // split elementUI into a single package
+                                ui: {
+                                    name: 'chunk-ui', // split elementUI into a single package
                                     priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
                                     test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
                                 }
