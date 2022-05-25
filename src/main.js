98c96adcf3f8b2cd1be75fb6358d3825757313fc
tower1229 <tower1229@foxmail.com>
2022-05-25 09:10:37 +0800

    mock

--- src/main.js
+++ src/main.js
@@ -1,36 +1,36 @@
 // 开启 mock
-// import mocker from '../mock';
-// mocker()
+import mocker from "../mock";
+mocker();
 
-import Vue from 'vue';
+import Vue from "vue";
 
 // 加载根组件
-import App from './App.vue';
+import App from "./App.vue";
 
 // 创建路由实例
-import { routeGenerator } from '@/core';
+import { routeGenerator } from "@/core";
 const routeInstance = routeGenerator({
-    beforeEach: ((to, from, next) => {
-        if (to.name) {
-            document.title = to.meta.title || to.name;
-        }
-        next()
-    })
-})
+  beforeEach: (to, from, next) => {
+    if (to.name) {
+      document.title = to.meta.title || to.name;
+    }
+    next();
+  },
+});
 
 // 预安装
-import preInstall from '@/pre-install';
+import preInstall from "@/pre-install";
 Vue.use(preInstall);
 
 // 加载核心插件
-import plugins from '@/core/plugins';
+import plugins from "@/core/plugins";
 Vue.use(plugins);
 
 // 创建应用
 const app = new Vue({
-    router: routeInstance,
-    render: h => h(App)
-})
+  router: routeInstance,
+  render: (h) => h(App),
+});
 
 // 挂载应用
-app.$mount('#app');
+app.$mount("#app");
