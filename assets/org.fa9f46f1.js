7895c3f5f9bbcfbb634fb5c96cedebe6a0c767e5
tower1229 <tower1229@foxmail.com>
2022-06-20 09:33:23 +0000

    Deploying to gh-pages from @ cutting-mat/template-element-ui@30d44aaeecc8843f03bdd0c6f3cbcee9aff12239 🚀

--- assets/org.fa9f46f1.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./index.d870fe9f.js";const r=e=>t.get("/orgs",{params:e}),o=e=>t.get("/org",{params:e}),s=e=>t.post("/org",e),n=e=>t.put("/org",e),a=e=>t.delete("/org",{params:e});var d=Object.freeze(Object.defineProperty({__proto__:null,list:r,get:o,add:s,edit:n,remove:a},Symbol.toStringTag,{value:"Module"}));export{s as a,n as e,r as l,d as o,a as r};
