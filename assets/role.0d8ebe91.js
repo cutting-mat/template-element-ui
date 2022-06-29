d830b1e89ab6e9b618f3436f50b8081050303185
tower1229 <tower1229@foxmail.com>
2022-06-29 09:28:02 +0000

    Deploying to gh-pages from @ cutting-mat/template-element-ui@13fb0af208a2e6fefc6d90a963fd04db3092ab27 🚀

--- assets/role.0d8ebe91.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./index.c0cac681.js";const a=a=>e.get("/role/s",{params:a}),r=a=>e.post("/role",a),o=a=>e.put("/role",a),t=a=>e.delete("/role",{params:a});var l=Object.freeze(Object.defineProperty({__proto__:null,list:a,detail:a=>e.get("/role",{params:a}),add:r,edit:o,remove:t},Symbol.toStringTag,{value:"Module"}));export{r as a,t as b,o as e,a as l,l as r};
---
+++ assets/role.3319e035.js
@@ -0,0 +1,1 @@
+import{i as e}from"./index.edf7f234.js";const r=r=>e.get("/role/s",{params:r}),a=r=>e.post("/role",r),o=r=>e.put("/role",r),t=r=>e.delete("/role",{params:r});var l=Object.freeze(Object.defineProperty({__proto__:null,list:r,detail:r=>e.get("/role",{params:r}),add:a,edit:o,remove:t},Symbol.toStringTag,{value:"Module"}));export{a,t as b,o as e,r as l,l as r};
